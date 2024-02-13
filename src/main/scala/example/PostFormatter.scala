60fffaea70c9a7f78a8fa1f19021b65d086b3a02
maiksteffan <44241462+maiksteffan@users.noreply.github.com>
2024-02-13 12:21:49 +0100

    postformat update - ignore emty text

--- src/main/scala/example/PostFormatter.scala
+++ src/main/scala/example/PostFormatter.scala
@@ -12,25 +12,28 @@ class PostFormatter {
       val category = (data \ "subreddit").as[String]
       val text = (data \ "selftext").as[String]
 
-      val createdUtc = (data \ "created_utc").as[Long]
-      val instant = Instant.ofEpochSecond(createdUtc)
-      val zonedDateTime = ZonedDateTime.ofInstant(instant, ZoneId.systemDefault())
-      val formattedDate = zonedDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+      if (ignoreEmptySelftext && text.isEmpty) {
+        None // Skip post if ignoreEmptySelftext is true and selftext is empty
+      } else {
+        val createdUtc = (data \ "created_utc").as[Long]
+        val instant = Instant.ofEpochSecond(createdUtc)
+        val zonedDateTime = ZonedDateTime.ofInstant(instant, ZoneId.systemDefault())
+        val formattedDate = zonedDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
 
-      Some(
-        Json.obj(
-          "_id" -> JsString((data \ "id").as[String]),
-          "url" -> JsString((data \ "url").as[String]),
-          "title" -> JsString((data \ "title").as[String]),
-          "date" -> JsString(formattedDate),
-          "category" -> JsString(category),
-          "comments" -> JsNumber((data \ "num_comments").as[Int]),
-          "text" -> JsString(text),
-          "source" -> "Reddit"
+        Some(
+          Json.obj(
+            "_id" -> JsString((data \ "id").as[String]),
+            "url" -> JsString((data \ "url").as[String]),
+            "title" -> JsString((data \ "title").as[String]),
+            "date" -> JsString(formattedDate),
+            "category" -> JsString(category),
+            "comments" -> JsNumber((data \ "num_comments").as[Int]),
+            "text" -> JsString(text),
+            "source" -> "Reddit"
+          )
         )
-      )
+      }
     }
     Json.toJson(formattedPosts)
   }
 }
-
